568b04f32224f1bfd9d60003cc083a4432a16cbb
anlt <thanhan.le21@gmail.com>
2021-02-17 15:40:28 +0700

    SignIn (validation)

--- lib/module/signin/signin_bloc.dart
+++ lib/module/signin/signin_bloc.dart
@@ -1,14 +1,62 @@
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_book_store_sample/base/base_bloc.dart';
 import 'package:flutter_book_store_sample/base/base_event.dart';
 import 'package:flutter_book_store_sample/data/repo/user_repo.dart';
 import 'package:flutter_book_store_sample/event/signup_event.dart';
 import 'package:flutter_book_store_sample/event/singin_event.dart';
+import 'package:flutter_book_store_sample/shared/validation.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SignInBloc extends BaseBloc {
+  final _phoneSubject = BehaviorSubject<String>();
+  final _passSubject = BehaviorSubject<String>();
+  final _btnSubject = BehaviorSubject<bool>();
+
   UserRepo _userRepo;
 
-  SignInBloc({@required UserRepo userRepo}) : _userRepo = userRepo;
+  SignInBloc({@required UserRepo userRepo}) {
+    _userRepo = userRepo;
+    validateForm();
+  }
+
+  var phoneValidation =
+      StreamTransformer<String, String>.fromHandlers(handleData: (phone, sink) {
+    if (Validation.isPhoneValid(phone)) {
+      sink.add(null);
+      return;
+    }
+    sink.add('Phone Invalid');
+  });
+
+  var passValidation =
+  StreamTransformer<String, String>.fromHandlers(handleData: (phone, sink) {
+    if (Validation.isPassValid(phone)) {
+      sink.add(null);
+      return;
+    }
+    sink.add('Password too short');
+  });
+
+  Stream<String> get phoneStream => _phoneSubject.stream.transform(phoneValidation);
+
+  Sink<String> get phoneSink => _phoneSubject.sink;
+
+  Stream<String> get passStream => _passSubject.stream.transform(passValidation);
+
+  Sink<String> get passSink => _passSubject.sink;
+
+  Stream<bool> get btnStream => _btnSubject.stream;
+
+  Sink<bool> get btnSink => _btnSubject.sink;
+
+  validateForm() {
+    Observable.combineLatest2(_phoneSubject, _passSubject, (phone, pass) {
+      return Validation.isPhoneValid(phone) && Validation.isPassValid(pass);
+    }).listen((enable) {
+      btnSink.add(enable);
+    });
+  }
 
   @override
   void dispatchEvent(BaseEvent event) {
@@ -40,5 +88,8 @@ class SignInBloc extends BaseBloc {
   void dispose() {
     // TODO: implement dispose
     super.dispose();
+    _phoneSubject.close();
+    _passSubject.close();
+    _btnSubject.close();
   }
 }
--- lib/module/signin/signin_page.dart
+++ lib/module/signin/signin_page.dart
@@ -41,24 +41,38 @@ class SignInFormWidget extends StatelessWidget {
       value: SignInBloc(userRepo: Provider.of(context)),
       child: Consumer<SignInBloc>(
         builder: (context, bloc, child) => Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _buildPhoneField(),
-              _buildPassField(),
-              NormalButton(
-                onPressed: () {
-                  bloc.event.add(
-                    SignInEvent(
-                      phone: _txtPhoneController.text,
-                      pass: _txtPassController.text,
-                    ),
-                  );
-                },
+          child: viewOriginTut(bloc),
+        ),
+      ),
+    );
+  }
+
+  Widget viewOriginTut(SignInBloc bloc) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildPhoneField(bloc),
+        _buildPassField(bloc),
+        _buildSignInButton(bloc),
+        _buildFooter()
+      ],
+    );
+  }
+
+  Widget _buildSignInButton(SignInBloc bloc){
+    return StreamProvider<bool>.value(
+      initialData: false,
+      value: bloc.btnStream,
+      child: Consumer<bool>(
+        builder: (context, enable, child) => NormalButton(
+          onPressed: enable ? () {
+            bloc.event.add(
+              SignInEvent(
+                phone: _txtPhoneController.text,
+                pass: _txtPassController.text,
               ),
-              _buildFooter()
-            ],
-          ),
+            );
+          } : null,
         ),
       ),
     );
@@ -77,37 +91,57 @@ class SignInFormWidget extends StatelessWidget {
     );
   }
 
-  Widget _buildPhoneField() {
-    return Container(
-      margin: EdgeInsets.only(bottom: 15),
-      child: TextFormField(
-        textInputAction: TextInputAction.next,
-        controller: _txtPhoneController,
-        cursorColor: Colors.black,
-        keyboardType: TextInputType.phone,
-        decoration: InputDecoration(
-          icon: Icon(Icons.phone, color: AppColor.blue),
-          hintText: 'Phone',
-          labelText: 'Phone',
-          labelStyle: TextStyle(color: AppColor.blue),
+  Widget _buildPhoneField(SignInBloc bloc) {
+    return StreamProvider<String>.value(
+      initialData: null,
+      value: bloc.phoneStream,
+      child: Consumer<String>(
+        builder: (context, msg, child) => Container(
+          margin: EdgeInsets.only(bottom: 15),
+          child: TextField(
+            textInputAction: TextInputAction.next,
+            onChanged: (text) {
+              bloc.phoneSink.add(text);
+            },
+            controller: _txtPhoneController,
+            cursorColor: Colors.black,
+            keyboardType: TextInputType.phone,
+            decoration: InputDecoration(
+              icon: Icon(Icons.phone, color: AppColor.blue),
+              hintText: 'Phone',
+              labelText: 'Phone',
+              errorText: msg,
+              labelStyle: TextStyle(color: AppColor.blue),
+            ),
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildPassField() {
-    return Container(
-      margin: EdgeInsets.only(bottom: 25),
-      child: TextFormField(
-        textInputAction: TextInputAction.done,
-        controller: _txtPassController,
-        obscureText: true,
-        cursorColor: Colors.black,
-        decoration: InputDecoration(
-          icon: Icon(Icons.lock, color: AppColor.blue),
-          hintText: 'Password',
-          labelText: 'Password',
-          labelStyle: TextStyle(color: AppColor.blue),
+  Widget _buildPassField(SignInBloc bloc) {
+    return StreamProvider<String>.value(
+      initialData: null,
+      value: bloc.passStream,
+      child: Consumer<String>(
+        builder: (context, msg, child) =>  Container(
+          margin: EdgeInsets.only(bottom: 25),
+          child: TextField(
+            textInputAction: TextInputAction.done,
+            onChanged: (text) {
+              bloc.passSink.add(text);
+            },
+            controller: _txtPassController,
+            obscureText: true,
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock, color: AppColor.blue),
+              hintText: 'Password',
+              labelText: 'Password',
+              errorText: msg,
+              labelStyle: TextStyle(color: AppColor.blue),
+            ),
+          ),
         ),
       ),
     );
